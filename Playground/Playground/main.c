78b77401e555c5c3b334d33ec4a5cb50b267dbe7
AOx0 <50227494+AOx0@users.noreply.github.com>
2020-08-09 03:54:36 -0500

    Repo init

---
+++ Playground/Playground/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Playground
+//
+//  Created by Alejandro D on 09/08/20.
+//
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("Lol\n");
+    return 0;
+}
